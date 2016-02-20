344c4fa4cbed1f4f42e8f129e579ed7be13bbde2
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-02-20 15:10:32 -0430

    Adelgazando navbar y agrandando su tipografia

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$83c9ef70a88248f8$bec724735f0b9a49e5f1936d4dff5c79997d4a1f"
-=======
-password="pbkdf2(1000,20,sha512)$964ebad0d1b549de$75fed4abb98f5e68b1567efc1fb01e35c1cda0af"
->>>>>>> devel
+password="pbkdf2(1000,20,sha512)$a67bdc42e70af052$481f6b4cd8644f38a720cabbc27a1c128b57f274"
