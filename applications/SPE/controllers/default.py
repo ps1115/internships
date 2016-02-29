ecfee38f154a85374db2ab16d0665da3f47a182f
al3x4nd3r <alexanderstower@gmail.com>
2016-02-29 01:24:26 +0000

    Se implementaron todas las operaciones CRUD con los catálogos. Faltan las configuraciones junto con sus respectivas tablas. Se ocultó el footer del layout en la vista gestion_cct2.html, pues ésta se solapaba con el grid.

--- applications/SPE/controllers/default.py
+++ applications/SPE/controllers/default.py
@@ -59,3 +59,88 @@ def call():
 
 def retirar_pasantia():
     return response.render('default/retirar_pasantia.html')
+
+
+def crear_estudiante():
+    form = SQLFORM(db.usuario_estudiante)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    
+    return form
+
+
+
+def crear_profesor():
+    form = SQLFORM(db.usuario_profesor)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    
+    return form
+
+
+def crear_empresa():
+    form = SQLFORM(db.empresa)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    
+    return form
+
+
+def crear_departamento():
+    form = SQLFORM(db.departamento)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    
+    return form
+
+
+def crear_carrera():
+    form = SQLFORM(db.carrera)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    
+    return form
+
+
+def estudiantes():
+    grid = SQLFORM.grid(db.usuario_estudiante,user_signature=False)
+    return grid
+
+def carreras():
+    grid = SQLFORM.grid(db.carrera,user_signature=False)
+    return grid
+
+def empresas():
+    grid = SQLFORM.grid(db.empresa,user_signature=False)
+    return grid
+
+def profesores():
+    grid = SQLFORM.grid(db.usuario_profesor,user_signature=False)
+    return grid
+
+def departamentos():
+    grid = SQLFORM.grid(db.departamento,user_signature=False)
+    return grid
+
+def gestion_cct2():
+    return dict()
