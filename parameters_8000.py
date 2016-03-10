de54f01d900d5b57a56e912fd2c975e2fafe7ab9
Leonardo Martinez <martinezazuaje@gmail.com>
2016-03-10 01:44:04 -0430

    Corregido detalle con la vista de inicio de sesion de Empresas.

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8a3b577cd1ce49d5$fbdd5c58c0f3516221cda8a437151c913bfe6d0d"
+password="pbkdf2(1000,20,sha512)$a3b36a7c8c832b96$14375ffc7b59f6ebc7f54ad5612bd0c0906917fa"
