3e8dc0b3406e159a679576f635a78e4be77fea8a
Francisco Javier Sucre González <frank91frank@gmail.com>
2016-03-15 17:54:11 -0430

    Archivo para crear usuarios

---
+++ data-bases/usuarioSPE.sql
@@ -0,0 +1,34 @@
+START TRANSACTION;
+
+	-- Crea el usuario
+	
+	-- El % se cambiara por la direccion IP con la cual se accedera
+	-- a la aplicacion
+	
+	-- El alist_development se cambiara por el nombre que tenga la
+	-- base de datos con la cual corra la aplicacion
+	
+	CREATE USER 'spe'@'%' IDENTIFIED BY 'spe'; 
+	
+	-- Permiso para obtener filas de las tablas
+	
+	GRANT SELECT ON spe.* TO 'spe'@'%';
+	
+	-- Permiso para agregar filas a las tablas
+	
+	GRANT INSERT ON spe.* TO 'spe'@'%';
+	
+	-- Permiso para eliminar filas de las tablas
+	
+	GRANT UPDATE ON spe.* TO 'spe'@'%';
+	
+	-- Permiso para ejecutar funciones o procedimientos
+	
+	GRANT EXECUTE ON spe.* TO 'spe'@'%';
+	
+	-- Para mostrar el usuario, en los privilegios sale la N como si el usuario no los
+	-- tuviese pero esto es porque solo los tiene para una base de datos en especifico
+	
+	SELECT * FROM mysql.user;
+
+COMMIT;
