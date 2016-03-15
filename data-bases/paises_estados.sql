d798c1cdae793debcfc3a49074b12c481673317b
Francisco Javier Sucre González <frank91frank@gmail.com>
2016-03-15 16:10:28 -0430

    Se agrego unscript para agregar paises y estados a la aplicacion

---
+++ data-bases/paises_estados.sql
@@ -0,0 +1,32 @@
+INSERT INTO `pasantiasnuevo`.`pais`
+(`nombre`)
+VALUES ('Venezuela');
+
+INSERT INTO `pasantiasnuevo`.`pais`
+(`nombre`)
+VALUES ('U.S.A.');
+
+INSERT INTO `pasantiasnuevo`.`estado`
+(`nombre`,`id_pais`)
+VALUES
+('Distrito Capital',1);
+
+INSERT INTO `pasantiasnuevo`.`estado`
+(`nombre`,`id_pais`)
+VALUES
+('Miranda',1);
+
+INSERT INTO `pasantiasnuevo`.`estado`
+(`nombre`,`id_pais`)
+VALUES
+('Aragua',1);
+
+INSERT INTO `pasantiasnuevo`.`estado`
+(`nombre`,`id_pais`)
+VALUES
+('Aragua',1);
+
+INSERT INTO `pasantiasnuevo`.`estado`
+(`nombre`,`id_pais`)
+VALUES
+('Florida',2);
