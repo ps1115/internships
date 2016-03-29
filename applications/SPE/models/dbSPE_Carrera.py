862456d3df00ab63d312ebc9ab205efe369503e8
franciscosucre <frank91frank@gmail.com>
2016-03-29 13:17:44 -0430

    (FJSG) Una 'coma' extra estaba causando errores

--- applications/SPE/models/dbSPE_Carrera.py
+++ applications/SPE/models/dbSPE_Carrera.py
@@ -5,7 +5,7 @@ dbSPE.define_table('carrera',
                 Field('codigo','string',required=True, requires=[IS_LENGTH(4),IS_NOT_EMPTY(error_message='Campo Obligatorio'),IS_NOT_IN_DB(dbSPE, 'carrera.codigo',error_message=T('Carrera ya existe'))], ondelete='CASCADE', notnull=True, unique=True,label='Código'),
                 Field('nombre','string',required=True,unique=True, requires=[IS_LENGTH(100)], label='Nombre'),
                 Field('duracion','string',required=True, requires=IS_IN_SET(['Larga','Corta']), label="Duración", notnull=True),
-                Field('sede','string', requires=IS_IN_SET(['Sartenejas','Litoral'],,zero=None), label="Sede", notnull=True),
+                Field('sede','string', requires=IS_IN_SET(['Sartenejas','Litoral'],zero=None), label="Sede", notnull=True),
                 Field('coordinacion','refence coordinacion', label="Coordinación"),
                 primarykey=['codigo'],
                 format='%(codigo)s %(nombre)s'
