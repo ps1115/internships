5408dc12e3d8249688fcfdaa4181aa850dcd9a23
franciscosucre <frank91frank@gmail.com>
2016-03-30 01:11:00 -0430

    (FJSG) scripts para cambio de version

---
+++ actualizarBaseDeDatos.sh
@@ -0,0 +1,1 @@
+mysql --protocol=tcp -h localhost -P3306 -u spe2016 --password=root pasantiasnuevo < ./data-bases/SPE-DataBases.sql 
--- cleanDb.sh
+++ cleanDb.sh
@@ -1,3 +1,3 @@
 # Script que borra las base de datos de web2py para las aplicaciones Empresa y SPE
-rm applications/Empresas/databases/*
-rm applications/SPE/databases/*
+rm ./applications/Empresas/databases/*
+rm ./applications/SPE/databases/*
