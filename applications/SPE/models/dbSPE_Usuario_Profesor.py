406c76b3b822a1f2c4b72ee339bc336a5e288d1a
franciscosucre <frank91frank@gmail.com>
2016-04-14 21:29:51 -0430

    (FJSG) Se elimino el validador incorrecto del correo

--- applications/SPE/models/dbSPE_Usuario_Profesor.py
+++ applications/SPE/models/dbSPE_Usuario_Profesor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Estudiante
 dbSPE.define_table('usuario_profesor',
-                Field('usbid_usuario','string', requires=[IS_NOT_EMPTY(), IS_MATCH('[0-9][0-9]-[0-9]{5}','USBID Inválido'),IS_NOT_IN_DB(dbSPE, 'usuario_profesor.usbid_usuario',error_message=T('Usuario ya existe'))], label='USBID', ondelete='CASCADE', notnull=True,unique=True),
+                Field('usbid_usuario','string', requires=[IS_NOT_EMPTY(), IS_MATCH('[a-zA-Z][a-zA-Z0-9_]*@usb.ve','USBID Inválido'),IS_NOT_IN_DB(dbSPE, 'usuario_profesor.usbid_usuario',error_message=T('Usuario ya existe'))], label='USBID', ondelete='CASCADE', notnull=True,unique=True),
                 Field('dependencia','reference departamento', represent=lambda x, row: (dbSPE(dbSPE.departamento.id==x)).select().first().nombre,label='Dependencia'),
                 Field('dedicacion','reference dedicacion',represent=lambda x, row: (dbSPE(dbSPE.dedicacion.id==x)).select().first().nombre,  label='Dedicación'),
                 Field('categoria','reference categoria',represent=lambda x, row: (dbSPE(dbSPE.categoria.id==x)).select().first().nombre,  label='Categoría'),
