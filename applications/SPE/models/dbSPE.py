a3f67617250aa1bec3708f5b85b7525a4198c1e5
franciscosucre <frank91frank@gmail.com>
2016-04-14 21:09:58 -0430

    (FJSG) Se arreglo problema de que las empresas creadas por catalogo no podian iniciar sesion

--- applications/SPE/models/dbSPE.py
+++ applications/SPE/models/dbSPE.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-dbSPE = DAL('mysql://root:spe2016@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
+#dbSPE = DAL('mysql://root:spe2016@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
 #dbSPE = DAL('mysql://spe:spe2016@localhost:3306/spe', pool_size=0, migrate=False)
-#dbSPE = DAL('mysql://root:root@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
+dbSPE = DAL('mysql://root:root@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
