d3e11bbc81ef3050324cc7e92d7ebe908436d649
Francisco Javier Sucre González <frank91frank@gmail.com>
2016-05-01 20:14:00 -0430

    (FJSG) Se modifico el usuario de base de datos para unificarlo

--- applications/Empresas/models/dbSPE.py
+++ applications/Empresas/models/dbSPE.py
@@ -1,5 +1,2 @@
 # -*- coding: utf-8 -*-
-#dbSPE = DAL('mysql://root:www123456@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
-#dbSPE = DAL('mysql://root:root@localhost:3306/pasantiasnuevo', pool_size=0, migrate=False)
-dbSPE = DAL('mysql://root:spe2016@localhost:3306/pasantiasnuevo', pool_size=0,migrate=False)
-# dbSPE = DAL('mysql://spe:spe2016@localhost:3306/spe', pool_size=0,migrate=False)
+dbSPE = DAL('mysql://spe:spe2016@localhost:3306/spe', pool_size=0,migrate=False)
